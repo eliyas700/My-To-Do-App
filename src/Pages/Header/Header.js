edbbc549ad6d0676567078034c9d30fdfd5a17cc
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-19 02:54:12 +0600

    Navbar Changed If User is Availble

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,8 +1,12 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
   return (
     <div>
       <Navbar collapseOnSelect expand="lg">
@@ -19,9 +23,15 @@ const Header = () => {
               <Nav.Link as={Link} to="/addTask">
                 Add Task
               </Nav.Link>
-              <Nav.Link as={Link} to="/login">
-                Log In
-              </Nav.Link>
+              {user ? (
+                <Nav.Link onClick={() => signOut(auth)}>
+                  <small>{user?.displayName}</small> Sign Out
+                </Nav.Link>
+              ) : (
+                <Nav.Link as={Link} to="/login">
+                  Log In
+                </Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
